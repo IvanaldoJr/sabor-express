c79d1393d3b712b28517c4785235ef69a11dabb3
Ivanaldo Junior <juniorchagas@gazetaweb.com>
2024-03-08 17:54:09 -0300

    sem alteração

--- app.py
+++ app.py
@@ -8,10 +8,19 @@ print("""'
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       '""")
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
+print('1. Cadastrar Restaurantes')
+print('2. Listar Restaurantes')
+print('3. Ativar Restaurantes')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}!')
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+
+if opcao_escolhida == 1:
+    print('Cadastrar Restaurantes')
+elif opcao_escolhida == 2:
+    print('Listar Restaurantes') 
+elif opcao_escolhida == 3:
+    print('Ativar Restaurantes')
+else:
+    print('Encerrando o programa')          
